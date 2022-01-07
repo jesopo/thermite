b94d1ad95124da6200f624c644c310b7266f2daf
jesopo <github@lolnerd.net>
2022-01-07 21:18:46 +0000

    hold backlog even after we've replayed it

--- thermite/__init__.py
+++ thermite/__init__.py
@@ -2,9 +2,10 @@ import time
 from collections import deque
 from random import choice as random_choice
 from string import hexdigits
-from typing import Deque, Dict, Sequence
+from typing import Deque, Dict, Sequence, Set
 
 from irctokens import build, Line
+from ircstates import User
 from ircrobots import Bot as BaseBot
 from ircrobots import Server as BaseServer
 
@@ -16,6 +17,7 @@ from .config import Config
 from .database import Database
 
 CAP_OPER = Capability(None, "solanum.chat/oper")
+BACKLOG_MAX = 64
 
 
 class Server(BaseServer):
@@ -29,7 +31,7 @@ class Server(BaseServer):
 
         self._source_map: Dict[str, str] = {}
         self._target_map: Dict[str, str] = {}
-        self._source_log: Dict[str, Deque[str]] = {}
+        self._backlog: Dict[str, Deque[str]] = {}
 
         self._last_users = self.users.copy()
 
@@ -37,32 +39,39 @@ class Server(BaseServer):
         # turn off throttling
         pass
 
+    def _human_users(self, channel: str) -> Set[User]:
+        cusers = self.channels[self.casefold(channel)].users
+        users = {self.users[n] for n in cusers.keys()}
+        for user in list(users):
+            # TODO: don't hardcode services.libera.chat
+            if (
+                user.hostname == "services.libera.chat"
+                or user.nickname == self.nickname
+            ):
+                users.remove(user)
+        return users
+
     async def _send_log(self, out: str):
         await self.send(build("NOTICE", [self._config.channel, out]))
 
-    async def _send_source_log(self, source: str, out: str):
-        target = self._source_map[source]
-        target_users = {
-            self.users[n] for n in self.channels[self.casefold(target)].users.keys()
-        }
-        for target_user in list(target_users):
-            # TODO: don't hardcode services.libera.chat
-            if (
-                target_user.hostname == "services.libera.chat"
-                or target_user.nickname == self.nickname
-            ):
-                target_users.remove(target_user)
+    async def _log_backlog(self, target: str, out: str):
+        offset = len(f":{self.hostmask()} NOTICE {target} :")
+        while out:
+            out_take = out[: 510 - offset]
+            out = out[len(out_take) :]
+            await self.send(build("NOTICE", [target, out_take]))
 
-        if target_users:
-            offset = len(f":{self.hostmask()} NOTICE {target} :")
-            while out:
-                out_take = out[: 510 - offset]
-                out = out[len(out_take) :]
-                await self.send(build("NOTICE", [target, out_take]))
-        else:
-            if not source in self._source_log:
-                self._source_log[source] = deque()
-            self._source_log[source].append(out)
+    async def _add_backlog(self, source: str, out: str):
+        if not source in self._backlog:
+            self._backlog[source] = deque()
+
+        self._backlog[source].append(out)
+        if len(self._backlog[source]) > BACKLOG_MAX:
+            self._backlog[source].popleft()
+
+        target = self._source_map[source]
+        if self._human_users(target):
+            await self._log_backlog(target, out)
 
     async def line_read(self, line: Line):
         now = time.monotonic()
@@ -102,14 +111,14 @@ class Server(BaseServer):
         elif (
             line.command == "JOIN"
             and line.params[0] in self._target_map
-            and self._target_map[line.params[0]] in self._source_log
+            and len(self._human_users(line.params[0])) == 1
         ):
-            # someone joined a target channel and we've buffered up source
-            # logs because the target channel was thus far empty
-            source = self._target_map[line.params[0]]
-            while self._source_log[source]:
-                await self._send_source_log(source, self._source_log[source].popleft())
-            del self._source_log[source]
+            # target channel was empty until this join and we have a backlog.
+            # replay it for the joining user
+            target = line.params[0]
+            source = self._target_map[target]
+            for out in self._backlog[source]:
+                await self._log_backlog(target, out)
 
         elif (
             line.command in {"PRIVMSG", "NOTICE"}
@@ -139,18 +148,18 @@ class Server(BaseServer):
                 message = message.strip("\x01")
                 message = f"CTCP {message}"
 
-            await self._send_source_log(source, f"{who_str} {message}")
+            await self._add_backlog(source, f"{who_str} {message}")
 
         elif line.command == "MODE" and line.params[0] in self._source_map:
             source = line.params[0]
             args = " ".join(line.params[2:])
-            await self._send_source_log(
+            await self._add_backlog(
                 source, f"- {line.source} set mode {line.params[1]} {args}"
             )
 
         elif line.command in {"JOIN", "PART"} and line.params[0] in self._source_map:
             source = line.params[0]
-            await self._send_source_log(
+            await self._add_backlog(
                 source,
                 f"- {line.source} {line.command.lower()}ed {source}",
             )
@@ -161,7 +170,7 @@ class Server(BaseServer):
         ):
             message = f"- {line.source} changed nick to {line.params[0]}"
             for chan in common:
-                await self._send_source_log(chan, message)
+                await self._add_backlog(chan, message)
 
         elif (
             line.command == "QUIT"
@@ -175,7 +184,7 @@ class Server(BaseServer):
             if len(line.params) > 0:
                 message += f" ({line.params[0]})"
             for chan in sorted(common):
-                await self._send_source_log(chan, message)
+                await self._add_backlog(chan, message)
 
     async def cmd(
         self,
@@ -263,7 +272,7 @@ class Server(BaseServer):
             return []
 
     async def cmd_pipes(self, channel: str, sargs: str) -> Sequence[str]:
-        pipes = await self._database.get_pipes()
+        pipes = list(await self._database.get_pipes())
         pipes.sort()
         colmax = max([len(s) for s, t in pipes] or [0])
         return [f"{s.rjust(colmax)} -> {t}" for s, t in pipes] or ["no pipes"]
